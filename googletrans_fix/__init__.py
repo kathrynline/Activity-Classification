7b1df0c6e498a75512da2394a6dd0382095117c5
Emily Linebarger <eklinebarg@gmail.com>
2019-02-25 10:19:17 -0800

    Adding googletrans_fix package that Naomi was using into this repo

---
+++ googletrans_fix/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '2.3.0'
+
+
+from googletrans_fix.client import Translator
+from googletrans_fix.constants import LANGCODES, LANGUAGES
